69d8ed0e7b65a4bbbd6b0ddd932ec825859bfd92
BebopBobby <bbbgalvin4@gmail.com>
2018-09-25 15:57:03 -0400

    classwork

--- src/main/java/company/Employee.java
+++ src/main/java/company/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private String firstName;
     private String lastName;
     private double employeeID;
+    public int bonus;
 
     public Employee(String firstName, String lastName, double employeeID) {
         this.firstName = firstName;
@@ -13,5 +14,13 @@ public class Employee {
 
     public int payCheck(){
         return 0;
+    }
+
+    public String bonusAlert(Employee e){
+        return e.getClass().getSimpleName() + " received a bonus of " + bonus + " dollars!";
+    }
+
+    public String printEmployeeFullName(Employee a){
+        return firstName + " " + lastName;
     }
 }
--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -2,16 +2,19 @@ package company;
 
 public class Main {
     public static void main(String[] args) {
-        SalariedEmployee john = new SalariedEmployee("John", "Smith", 123456, 1000);
-        CommissionedEmployee jane = new CommissionedEmployee("Jane", "Jones", 1234567, 50, 60);
-        BasePlusEmployee mike = new BasePlusEmployee("Mike", "Jones", 12345678, 60, 50, 1000);
-        HourlyEmployee jeff = new HourlyEmployee("Jeff", "Jones", 123456789, 40, 9);
+      Employee salariedEmployee = new SalariedEmployee("John", "Smith", 123456, 1000);
+      Employee commissionedEmployee = new CommissionedEmployee("Jane", "Jones", 1234567, 50, 60);
+      Employee basePlusEmployee = new BasePlusEmployee("Mike", "Jones", 12345678, 60, 50, 1000);
+      Employee hourlyEmployee = new HourlyEmployee("Jeff", "Jones", 123456789, 40, 9);
 
-        Employee[] array = new Employee[] {john, jane, mike, jeff};
-        String[] stringArray = {"The SalariedEmployee earned ", "The CommisionedEmployee earned ", "The BasePlusCommisionEmployee "};
+//        Employee[] array = new Employee[] {john, jane, mike, jeff};
+//        String[] stringArray = {"The SalariedEmployee earned ", "The CommisionedEmployee earned ", "The BasePlusCommisionEmployee "};
+//
+//        for(Employee e : array){
+//            System.out.println("The " + e.getClass().getSimpleName() + " earned " + e.payCheck());
+//        }
 
-        for(Employee e : array){
-            System.out.println("The " + e.getClass().getSimpleName() + " earned " + e.payCheck());
-        }
+        System.out.println(basePlusEmployee.printEmployeeFullName(basePlusEmployee));
+//        System.out.println(basePlusEmployee.bonusAlert(basePlusEmployee));
     }
 }
